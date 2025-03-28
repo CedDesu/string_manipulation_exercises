bd1458d24f4dea96ea570281e7c9dc6e23ba50e2
CedDesu <cedriqueorbista@gmail.com>
2025-03-28 23:31:06 +0800

    added and tested final code

--- capitalize_alternative.py
+++ capitalize_alternative.py
@@ -5,3 +5,9 @@ capitalized_text =
 
 print()
 """
+
+text = input("Enter a sentence: ")
+
+capitalized_text = (text[0].upper() + text[1:].lower()) if text else ""
+
+print("Result:", capitalized_text)
