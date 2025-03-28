1c34c0f52842c577c06863ed926f04c9c38ca9fc
CedDesu <cedriqueorbista@gmail.com>
2025-03-28 22:42:49 +0800

    Finalized code and transferred lowercase variable from pseudocode to final code

--- lower_case_alternative.py
+++ lower_case_alternative.py
@@ -1,7 +1,11 @@
 """
 input
 
-lowercase = ''.join(chr(ord(c) + 32) if 'A' <= c <= 'Z' else c for c in text)
-
 print
 """
+
+text = input("Enter a string: ")
+
+lowercase = ''.join(chr(ord(c) + 32) if 'A' <= c <= 'Z' else c for c in text)
+
+print("Result:", lowercase)
