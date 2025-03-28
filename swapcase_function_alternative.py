b940e059c03edf27c8b758e96144c04c4d5311aa
CedDesu <cedriqueorbista@gmail.com>
2025-03-28 23:24:19 +0800

    added final code and made it appropriate for the swapcase conditions.

--- swapcase_function_alternative.py
+++ swapcase_function_alternative.py
@@ -6,3 +6,9 @@ lowercase = ''.join(chr(ord(c) + 32) if 'A' <= c <= 'Z' else c for c in text)
 
 print
 """
+
+text = input("Enter a sentence: ")
+
+swapcase_text = ''.join(chr(ord(c) + 32) if 'A' <= c <= 'Z' else chr(ord(c) - 32) if 'a' <= c <= 'z' else c for c in text)
+
+print("Result:", swapcase_text)
