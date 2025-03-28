4296d4a9d2b2b5a5fddccfaefad3b95eb190ec64
CedDesu <cedriqueorbista@gmail.com>
2025-03-28 16:49:41 +0800

    added code and tested for working condition

--- space_start_without_lstrip.py
+++ space_start_without_lstrip.py
@@ -7,3 +7,11 @@ use while and len to replace spaces with nothing
 print user input
 
 """
+
+text = input("Input a sentence: ")
+
+i = 0
+while i < len(text) and text[i] == ' ':
+    i += 1
+
+print(text[i:])
