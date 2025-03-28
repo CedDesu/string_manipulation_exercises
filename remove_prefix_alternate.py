7e6e165929b626758e751c42ed44f13bf39291e9
CedDesu <cedriqueorbista@gmail.com>
2025-03-28 17:07:38 +0800

    added final code and tested for working condition

--- remove_prefix_alternate.py
+++ remove_prefix_alternate.py
@@ -7,3 +7,10 @@ use if and len to remove prefix
 
 """
 
+text = input("Enter a string: ")
+prefix = input("Enter the prefix to remove: ")
+
+if text.startswith(prefix):
+    text = text[len(prefix):]
+
+print("Result:", text)
