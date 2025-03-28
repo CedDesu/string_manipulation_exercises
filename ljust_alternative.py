2c374b3026f5390e81327a9a56ecad2cd0ce6b4b
CedDesu <cedriqueorbista@gmail.com>
2025-03-28 23:08:20 +0800

    added final code using if and len and finalized working status

--- ljust_alternative.py
+++ ljust_alternative.py
@@ -7,3 +7,10 @@ ends_with
 print()
 """
 
+text = input("Enter a string: ")
+width = int(input("Enter the total width: "))
+
+if len(text) < width:
+    text = text + ' ' * (width - len(text))
+
+print("Result:", text)
