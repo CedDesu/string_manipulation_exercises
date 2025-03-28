bd4e88292f1f6892af8413822dee22c600c55ddb
CedDesu <cedriqueorbista@gmail.com>
2025-03-28 23:36:48 +0800

    added final working code and tested for appropriate function

--- title_function_alternative.py
+++ title_function_alternative.py
@@ -5,3 +5,9 @@ title_text =
 
 print()
 """
+
+text = input("Enter a sentence: ")
+
+title_text = ' '.join(word[0].upper() + word[1:].lower() if word else '' for word in text.split())
+
+print("Result:", title_text)
