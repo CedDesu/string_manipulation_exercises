f7008edfd2dda25fe8c099133e67014cdbc4f28f
CedDesu <cedriqueorbista@gmail.com>
2025-03-31 22:06:32 +0800

    added final code and tested for working condition

--- starts_with_alternative.py
+++ starts_with_alternative.py
@@ -7,3 +7,9 @@ starts_with
 print()
 """
 
+text = input("Enter a sentence: ")
+prefix = input("Enter the prefix to check: ")
+
+starts_with = text[:len(prefix)] == prefix if len(prefix) <= len(text) else False
+
+print("Does the string start with the prefix?", starts_with)
