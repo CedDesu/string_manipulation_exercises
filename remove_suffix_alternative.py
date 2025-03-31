b4d8de6ca7e9f3a4118173aec2a204a8968e1790
CedDesu <cedriqueorbista@gmail.com>
2025-03-31 21:31:27 +0800

    added similar code from previous removeprefix code and modified to remove suffix instead

--- remove_suffix_alternative.py
+++ remove_suffix_alternative.py
@@ -6,3 +6,11 @@ prefix = input ()
 use if and len to remove suffix
 
 """
+
+text = input("Enter a sentence: ")
+suffix = input("Enter the suffix to remove: ")
+
+if text.endswith(suffix):
+    text = text[:-len(suffix)]
+
+print("Result:", text)
