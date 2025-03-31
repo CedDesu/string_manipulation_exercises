e6fcf0d217b09c24c2e2fb7c65ea3226868aa94e
CedDesu <cedriqueorbista@gmail.com>
2025-03-31 21:56:09 +0800

    added final code and tested for working condition (changed 'a' <= c <= 'z')

--- islower_case_alternative.py
+++ islower_case_alternative.py
@@ -6,3 +6,8 @@ all_upper = all('A' <= c <= 'Z' for c in text if c.isalpha())
 print
 """
 
+text = input("Enter a string: ")
+
+all_lower = all('a' <= c <= 'z' for c in text if c.isalpha())
+
+print("Is the string all lowercase?", all_lower)
