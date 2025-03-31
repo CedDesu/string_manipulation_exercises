bac153072e018c53dc776a63e7ee985465a738e3
CedDesu <cedriqueorbista@gmail.com>
2025-03-31 21:49:06 +0800

    added final code and tested for proper capitalization of input letters

--- upper_case_alternative.py
+++ upper_case_alternative.py
@@ -6,3 +6,8 @@ lowercase = ''.join(chr(ord(c) + 32) if 'A' <= c <= 'Z' else c for c in text)
 print
 """
 
+text = input("Enter a string: ")
+
+lowercase = ''.join(chr(ord(c) - 32) if 'a' <= c <= 'z' else c for c in text)
+
+print("Result:", lowercase)
