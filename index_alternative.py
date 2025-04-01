c13095e53e4c1c0d3c3144125ee9012933d727fe
CedDesu <cedriqueorbista@gmail.com>
2025-04-01 21:00:54 +0800

    Wrote and tested final code for working condition

--- index_alternative.py
+++ index_alternative.py
@@ -6,3 +6,17 @@ position = -1
 
 print()
 """
+
+text = input("Enter a sentence: ")
+char = input("Enter the character to find: ")
+
+position = -1
+for i in range(len(text)):
+    if text[i] == char:
+        position = i
+        break
+
+if position != -1:
+    print(f"The first occurrence of '{char}' is at index {position}.")
+else:
+    print(f"'{char}' is not found in the user input.")
