6d9e7280fe2ebd64694710ae2670e729e3e9af21
CedDesu <cedriqueorbista@gmail.com>
2025-04-01 20:27:33 +0800

    Added final input and print code and tested for working condition

--- zfill_alternative.py
+++ zfill_alternative.py
@@ -9,3 +9,10 @@ print()
 """
 
 
+text = input("Enter a sentence: ")
+width = int(input("Enter the total width: "))
+
+if len(text) < width:
+    text = '0' * (width - len(text)) + text
+
+print("Result:", text)
