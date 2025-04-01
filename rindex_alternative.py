078c19a1034637a13a386e15af7cf5f250d275a8
CedDesu <cedriqueorbista@gmail.com>
2025-04-01 21:12:38 +0800

    wrote and modified the final code to find the last occurrence of the specified character to find

--- rindex_alternative.py
+++ rindex_alternative.py
@@ -10,5 +10,17 @@ for i in range(len(text)):
 
 print()
 """
+text = input("Enter a sentence: ")
+char = input("Enter the character to find: ")
 
+position = -1
+for i in range(len(text) - 1, -1, -1):
+    if text[i] == char:
+        position = i
+        break
+
+if position != -1:
+    print(f"The last occurrence of '{char}' is at index {position}.")
+else:
+    print(f"'{char}' is not found in the user input.")
 
